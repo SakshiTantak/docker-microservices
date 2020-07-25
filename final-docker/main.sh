6df0dcd7c868024223f98198cef99e56a5daddea
kesarling <uchchaydugal@hotmail.com>
2020-07-25 13:53:15 +0530

    JAVA environment variables have now been set!!

--- final-docker/main.sh
+++
@@ -1,7 +0,0 @@
-if [ -z ${DISPLAY+x} ]; then
-	/usr/local/apache-tomcat-v8.5.57/bin/catalina.sh run
-else
-	/usr/local/apache-tomcat-v8.5.57/bin/catalina.sh run&
-	bash /opt/eclipse/eclipse
-fi
-
--- final-docker/run.sh
+++ final-docker/run.sh
@@ -1,3 +1,3 @@
-docker container run -it --net=host -e DISPLAY=$DISPLAY --volume=$HOME/.Xauthority:/root/.Xauthority:rw microservices-docker /bin/bash
-# docker container run -it -p 8080:8080 microservices-docker /bin/bash
+# docker container run -it --net=host -e DISPLAY=$DISPLAY --volume=$HOME/.Xauthority:/root/.Xauthority:rw microservices-docker /bin/bash
+docker container run -it -p 8080:8080 microservices-docker
 
