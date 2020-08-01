c2770e7b8a77f14302abd7a259d7f56f1e6d26f1
kesarling <uchchaydugal@hotmail.com>
2020-08-01 17:53:55 +0530

    only catalina variables remain to be configured!!!!

---
+++ run.sh
@@ -0,0 +1,2 @@
+# docker container run -it --net=host -e DISPLAY=$DISPLAY --volume=$HOME/.Xauthority:/root/.Xauthority:rw microservices-docker /bin/bash
+docker container run -dp 8080:8080 microservices-docker
