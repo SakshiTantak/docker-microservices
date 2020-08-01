395f7abe489583cec56e1e81701f4e097edb0c4a
kesarling <uchchaydugal@hotmail.com>
2020-08-01 17:50:21 +0530

    Only tomcat variables remain to be configured

--- final-docker/run.sh
+++
@@ -1,3 +0,0 @@
-# docker container run -it --net=host -e DISPLAY=$DISPLAY --volume=$HOME/.Xauthority:/root/.Xauthority:rw microservices-docker /bin/bash
-docker container run -dp 8080:8080 microservices-docker
-
